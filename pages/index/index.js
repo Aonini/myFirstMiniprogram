4b9d603bc23f0c87b0f04e890806c64cab70a2bf
baicx@irisking.com <baicx@irisking.com>
2021-03-17 18:22:35 +0800

    myfirstcommit

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,9 +4,9 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
+    motto: '小白测试页面',
     userInfo: {},
-    hasUserInfo: false,
+    hasUserInfo: true,
     canIUse: wx.canIUse('button.open-type.getUserInfo'),
     canIUseGetUserProfile: false,
     canIUseOpenData: wx.canIUse('open-data.type.userAvatarUrl') && wx.canIUse('open-data.type.userNickName') // 如需尝试获取用户信息可改为false
